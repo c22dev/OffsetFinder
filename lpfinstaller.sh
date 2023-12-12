bbf3d769d1051716b13e8a3aaa9d78cf136f98c4
c22dev <102235607+c22dev@users.noreply.github.com>
2023-12-12 19:34:57 +0100

    added pzb

--- lpfinstaller.sh
+++ lpfinstaller.sh
@@ -2,6 +2,7 @@ clear
 echo "LibPatchFinderInstaller v0.2 - made by c22dev\nThis should help you install libpatchfinder. Please make sure brew and Xcode Command Line tools are installed.\nThis script needs sudo to work. Please enter your mac password."
 sudo echo "Sudoed successfuly !"
 echo "Installing autoconf, automake, libtool, pkg-config, openssl"
+# Brew some packages (those available)
 brew install autoconf
 brew install automake
 brew install libtool
@@ -19,42 +20,65 @@ read -p "Do you want to uninstall your current Python installation ? The script 
 mkdir workingLPFI
 cd workingLPFI
 echo "Cloning libpatchfinder and dependencies..."
+# clone all git repos
+# libpatchfinder is mine cause a fix wasnt pushed for a new version of libgeneral
 git clone --recursive https://github.com/c22dev/libpatchfinder
 git clone --recursive https://github.com/tihmstar/img4tool
 git clone --recursive https://github.com/tihmstar/img3tool
 git clone --recursive https://github.com/libimobiledevice/libplist
 git clone --recursive https://github.com/tihmstar/libgeneral
 git clone --recursive https://github.com/tihmstar/libinsn
+git clone --recursive https://github.com/tihmstar/libfragmentzip
+git clone --recursive https://github.com/tihmstar/partialZipBrowser
+# Libgeneral
 cd libgeneral
 ./autogen.sh
 sudo make
 sudo make install
 cd ..
+# Libinsn
 cd libinsn
 ./autogen.sh
 sudo make
 sudo make install
 cd ..
+# Libplist
 cd libplist
 ./autogen.sh
 sudo make
 sudo make install
 cd ..
+# Img4Tool
 cd img4tool
 ./autogen.sh
 sudo make
 sudo make install
 cd ..
+# Img3Tool
 cd img3tool
 ./autogen.sh
 sudo make
 sudo make install
 cd ..
+# libfragmentzip
+cd libfragmentzip
+./autogen.sh
+sudo make
+sudo make install
+cd ..
+# pzb
+cd partialZipBrowser
+./autogen.sh
+sudo make
+sudo make install
+cd ..
+# libpatchfinder
 cd libpatchfinder
 ./autogen.sh
 ./configure --with-offsetexporter
 sudo make
 sudo make install
 cd ..
+# Clean folder
 rm -rf workingLPFI
 echo "It should be installed ! Try running the command offsetexporter"
